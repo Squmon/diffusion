a0493891808d06a3e2ad53e07b6cfe862885d330
Squmon <squmon074@gmail.com>
2026-03-24 01:17:25 +0500

    Add initial implementation of diffusion processes and utility functions
    - Created a new Jupyter notebook `just_some_diffusion.ipynb` with basic structure and functions for forward processes and Langevin sampling.
    - Implemented a `forward_process` class and a derived `VP_SDE` class in `utils.py` to handle the forward diffusion process.
    - Added methods for noise generation and mapping functions within the `VP_SDE` class.
    - Included a Langevin sampling function for generating samples from the learned distribution.

---
+++ score_based_toy_dataset/working framework/utils.py
@@ -0,0 +1,45 @@
+import tensorflow as tf
+import keras
+
+class forward_process:
+    def forward(self, t, x0):  # возвращает зашумленную версию и шум
+        raise NotImplementedError
+
+    def map_function(self, x, y=None, w=None):
+        t = tf.random.uniform([], 0, 1)
+        noised, noise = self.forward(t, x)
+        if y is None:
+            return (t, noised), noise, w
+        else:
+            return (t, y, noised), noise, w
+
+class VP_SDE(forward_process):
+    def __init__(self, betas=None):
+        super().__init__()
+        if betas is None:
+            betas = tf.linspace(0.001, 0.02, 100)
+        self.betas = betas
+        self.num_steps = self.betas.shape[0]
+        self.alphas = 1.0 - self.betas
+        self.alphas_bar = tf.math.cumprod(self.alphas)
+
+    def sample_weight(self, t):
+        index = tf.squeeze(tf.cast(tf.math.floor(t * (self.num_steps - 1)), tf.int32))
+        return 1 - self.alphas_bar[index]
+
+    def forward(self, t, x0):
+        index = tf.squeeze(tf.cast(tf.math.floor(t * (self.num_steps - 1)), tf.int32))
+        noise = tf.random.normal(tf.shape(x0))
+        noised = x0 * tf.sqrt(self.alphas_bar[index]) + noise * tf.sqrt(
+            1 - self.alphas_bar[index]
+        )
+        score_function = - noise / tf.sqrt(1 - self.alphas_bar[index])
+        return noised, score_function
+
+def langevin_sampling(x_shape, score_function, steps, noise_scale = 1.0):
+    T = tf.linspace(1, 0, steps)
+    dt = 1/steps
+    x = tf.random.normal(x_shape)
+    for t in T:
+        x = x + dt*score_function(x = x, t = t) + noise_scale*tf.sqrt(2*dt) * tf.random.normal(x_shape)
+    return x
